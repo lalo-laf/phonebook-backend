2a5c37763a6409a943522f2b699802cc215dc20f
Leandro Martín Lafuente <llafuente@dc.uba.ar>
2024-03-12 19:21:54 -0300

    ejercicio 3.3

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toString()}</p>`)
 })
